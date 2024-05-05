8ad6ea8e65beea7b5fedfd0ae26c589dae0ba155
Dan Lourenco <danlourenco@gmail.com>
2024-05-05 15:43:01 -0400

    remove commented code

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,6 @@
 <template>
   <!-- Container -->
   <div class="flex flex-col h-lvh">
-    <!-- Nav -->
-    <!-- <nav class="navbar">
-      <div class="flex-1">
-        <NuxtLink to="/" class="btn btn-ghost text-xl"
-          >MoMA |
-          <span class="text-gray-500 text-xs">Museum of Munchkin Art</span>
-        </NuxtLink>
-      </div>
-      <div class="flex-none">
-        <ul class="menu menu-horizontal px-1">
-          <li><NuxtLink to="/about">About</NuxtLink></li>
-          <li><NuxtLink to="/upload">Upload</NuxtLink></li>
-          <li><NuxtLink to="/gallery">Gallery</NuxtLink></li>
-        </ul>
-      </div>
-    </nav> -->
     <div class="navbar bg-base-100">
       <div class="navbar-start">
         <div class="dropdown">
