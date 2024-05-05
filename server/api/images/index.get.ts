1ed86c7e45127352f565ade7afa3d2c9feeea553
Dan Lourenco <danlourenco@gmail.com>
2024-05-05 11:07:59 -0400

    remove image api; unneeded

--- server/api/images/index.get.ts
+++
@@ -1,8 +0,0 @@
-import { consola } from "consola";
-export default defineEventHandler(async (event) => {
-  const objects = await event.context.cloudflare.env.MEDIA_BUCKET.list();
-  consola.info(objects);
-  return {
-    objects,
-  };
-});
