2b2b93650860a97999cba66449c7f0ace12b6263
Dan Lourenco <danlourenco@gmail.com>
2024-05-05 15:41:48 -0400

    fix navbar on mobile, image sizing on about page, hide upload page

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <!-- Container -->
   <div class="flex flex-col h-lvh">
     <!-- Nav -->
-    <nav class="navbar">
+    <!-- <nav class="navbar">
       <div class="flex-1">
         <NuxtLink to="/" class="btn btn-ghost text-xl"
           >MoMA |
@@ -16,7 +16,48 @@
           <li><NuxtLink to="/gallery">Gallery</NuxtLink></li>
         </ul>
       </div>
-    </nav>
+    </nav> -->
+    <div class="navbar bg-base-100">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabindex="0"
+            class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-white rounded-box w-52"
+          >
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+            <li><NuxtLink to="/gallery">Gallery</NuxtLink></li>
+          </ul>
+        </div>
+        <NuxtLink to="/" class="btn btn-ghost text-xl"
+          >MoMA
+          <span class="text-gray-500 text-xs hidden lg:flex"
+            >| Museum of Munchkin Art</span
+          >
+        </NuxtLink>
+      </div>
+      <div class="navbar-end hidden lg:flex">
+        <ul class="menu menu-horizontal px-1">
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/gallery">Gallery</NuxtLink></li>
+        </ul>
+      </div>
+    </div>
     <!-- Main container -->
     <main class="grow p-6">
       <slot />
--- pages/about.vue
+++ pages/about.vue
@@ -5,7 +5,7 @@
       <img
         src="/images/Rui.jpg"
         alt="RA Lourenço demonstrating holding up his artwork and smiling"
-        class="-rotate-1 drop-shadow-2xl w-1/4 min-w-[300px]"
+        class="-rotate-1 drop-shadow-2xl w-1/4 min-w-[200px]"
       />
       <p>
         The Museum of Munchkin Art is a collection of art created by this kiddo.
