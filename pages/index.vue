077babc158d990eacd16c266a5272b0eb9cd26e2
Dan Lourenco <danlourenco@gmail.com>
2024-05-14 12:33:27 -0400

    remove commented code

--- pages/index.vue
+++ pages/index.vue
@@ -11,8 +11,4 @@
       </div>
     </div>
   </div>
-  <!-- <main class="flex flex-col items-center justify-center h-3/4">
-    <h1 class="text-8xl font-extrabold">MoMA</h1>
-    <p class="text-4xl">Museum of Munchkin Art</p>
-  </main> -->
 </template>
