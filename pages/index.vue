f56369fd191cacbb5b388891042ec0c0da3eb64c
Dan Lourenco <danlourenco@gmail.com>
2024-05-14 12:32:19 -0400

    add hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,18 @@
 <template>
-  <main class="flex flex-col items-center justify-center h-3/4">
+  <div
+    class="hero min-h-[95%]"
+    style="background-image: url(/images/splash.webp)"
+  >
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="text-8xl font-extrabold text-white">MoMA</h1>
+        <p class="text-4xl text-white">Museum of Munchkin Art</p>
+      </div>
+    </div>
+  </div>
+  <!-- <main class="flex flex-col items-center justify-center h-3/4">
     <h1 class="text-8xl font-extrabold">MoMA</h1>
     <p class="text-4xl">Museum of Munchkin Art</p>
-  </main>
+  </main> -->
 </template>
